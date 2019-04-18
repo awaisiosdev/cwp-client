538842e8d2d011f475b3b5dd38d2885ff5dcc521
Awais Aslam <aaslam.dev@icloud.com>
2019-04-18 11:42:08 +0300

    Doings 6 - Implementation Updates
    - Added UI to change frequency rapidly.
    - Some refactor.
    - Fixed the problem, where keyboard popup was hiding the toolbar.

--- app/src/main/java/esde06/tol/oulu/fi/ControlFragment.java
+++ app/src/main/java/esde06/tol/oulu/fi/ControlFragment.java
@@ -3,13 +3,18 @@ package esde06.tol.oulu.fi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
+import android.view.inputmethod.InputMethodManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.KeyEvent;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.ToggleButton;
 import android.widget.Toast;
 import android.preference.PreferenceManager;
@@ -21,11 +26,13 @@ import java.util.Observer;
 import esde06.tol.oulu.fi.cwprotocol.CWPControl;
 import esde06.tol.oulu.fi.cwprotocol.CWProtocolListener.CWPEvent;
 
-public class ControlFragment extends Fragment implements View.OnTouchListener, Observer, SharedPreferences.OnSharedPreferenceChangeListener {
+public class ControlFragment extends Fragment implements View.OnTouchListener, TextView.OnEditorActionListener, Observer, SharedPreferences.OnSharedPreferenceChangeListener {
 
     private final static String TAG = "ControlFragment";
     CWPControl control;
     ToggleButton connectionSwitch;
+    EditText frequencyValue;
+    Button changeFrequency;
     SharedPreferences preferences;
     private String serverAddressKey;
     private String serverPortKey;
@@ -45,17 +52,24 @@ public class ControlFragment extends Fragment implements View.OnTouchListener, O
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View fragmentLayout = inflater.inflate(R.layout.fragment_control, container, false);
-        connectionSwitch = fragmentLayout.findViewById(R.id.connectionSwitch);
-        connectionSwitch.setOnTouchListener(this);
-
-        connectionSwitch.setChecked(control.isConnected());
-
-        preferences = PreferenceManager.getDefaultSharedPreferences(this.getContext());
-        preferences.registerOnSharedPreferenceChangeListener(this);
 
         serverAddressKey = getString(R.string.pref_key_server_address);
         serverPortKey = getString(R.string.pref_key_server_port);
         connectionFrequencyKey = getString(R.string.pref_key_connection_frequency);
+
+        connectionSwitch = fragmentLayout.findViewById(R.id.connectionSwitch);
+        connectionSwitch.setOnTouchListener(this);
+        frequencyValue = fragmentLayout.findViewById(R.id.frequencyValue);
+        frequencyValue.setOnEditorActionListener(this);
+        changeFrequency = fragmentLayout.findViewById(R.id.changeFrequency);
+        changeFrequency.setOnTouchListener(this);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+        preferences.registerOnSharedPreferenceChangeListener(this);
+
+        connectionSwitch.setChecked(control.isConnected());
+        frequencyValue.setText(preferences.getString(connectionFrequencyKey, "-1"));
+
         return fragmentLayout;
     }
 
@@ -80,19 +94,36 @@ public class ControlFragment extends Fragment implements View.OnTouchListener, O
     public boolean onTouch(View v, MotionEvent event) {
         if (v instanceof ToggleButton && event.getAction() == MotionEvent.ACTION_DOWN){
                 if (!control.isConnected()){
-                    String serverAddress = preferences.getString(serverAddressKey, "0.0.0.0");
-                    String serverPort = preferences.getString(serverPortKey, "20000");
-                    String frequency = preferences.getString(connectionFrequencyKey, "-1");
-
-                    Log.d(TAG, "Connect to protocol server request initiated.");
-                    Log.d(TAG, "Connecting to " + serverAddress + ":" + serverPort + " at frequency: " + frequency);
-
-                    control.connect(serverAddress, Integer.parseInt(serverPort), Integer.parseInt(frequency));
+                    connect();
                 } else {
                     disconnect();
                 }
+        } else if (v instanceof Button && event.getAction() == MotionEvent.ACTION_DOWN){
+            changeFrequency();
+            // Hide the soft keyboard
+            InputMethodManager imm = (InputMethodManager) this.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(frequencyValue.getWindowToken(), 0);
         }
         return true;
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            changeFrequency();
+        }
+        return false;
+    }
+
+    private void connect(){
+        String serverAddress = preferences.getString(serverAddressKey, "0.0.0.0");
+        String serverPort = preferences.getString(serverPortKey, "20000");
+        String frequency = preferences.getString(connectionFrequencyKey, "-1");
+
+        Log.d(TAG, "Connect to protocol server request initiated.");
+        Log.d(TAG, "Connecting to " + serverAddress + ":" + serverPort + " at frequency: " + frequency);
+
+        control.connect(serverAddress, Integer.parseInt(serverPort), Integer.parseInt(frequency));
     }
 
     private void disconnect(){
@@ -102,6 +133,17 @@ public class ControlFragment extends Fragment implements View.OnTouchListener, O
         } catch (IOException e){
 
         }
+    }
+
+    private void changeFrequency(){
+
+        // Get the new frequency value
+        int newFrequency = Integer.valueOf(frequencyValue.getText().toString());
+
+        // store the new frequency value in the preferences.
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putString(connectionFrequencyKey,Integer.toString(newFrequency));
+        edit.apply();
     }
 
     @Override
@@ -125,6 +167,10 @@ public class ControlFragment extends Fragment implements View.OnTouchListener, O
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         Log.d(TAG, "Preference changed - " + key);
-        disconnect();
+        if (key != connectionFrequencyKey){
+            if (control.isConnected()){
+                disconnect();
+            }
+        }
     }
 }
